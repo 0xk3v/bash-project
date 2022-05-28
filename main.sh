b87dc8cf71e1292a2ddd3b542a379af358a6420e
n-kev1n <kevin_@tuta.io>
2022-05-28 13:19:46 +0200

    implemented create records feature

--- main.sh
+++ main.sh
@@ -9,6 +9,38 @@ function help_menu() {
   echo "    --search: To Search for a Record"
 }
 
+function check_dir() {
+  # Function to check if there's an available directory for storing records
+  BASEDIR="db_records"
+  if [[ -d "$BASEDIR" ]]; then
+    true
+  else
+    mkdir $BASEDIR
+  fi
+}
+check_dir
+
+function create_records() {
+  echo "Enter the Author:"
+  read -r AUTHOR
+  echo "Enter the Title:"
+  read -r TITLE
+  echo "Enter the Pages:"
+  read -r PAGES
+  echo "Enter the Year:"
+  read -r YEAR
+
+  local RECORDNAME=$(echo "$TITLE $PAGES$YEAR.txt" | tr 'A-Z' 'a-z' | tr ' ' '_')
+
+  # Outputting Records to a file
+  echo "Author: $AUTHOR" >>$BASEDIR/$RECORDNAME
+  echo "Title: $TITLE" >>$BASEDIR/$RECORDNAME
+  echo "Pages: $PAGES" >>$BASEDIR/$RECORDNAME
+  echo "Year: $YEAR" >>$BASEDIR/$RECORDNAME
+
+  echo "Records Successfully Created"
+}
+
 if [[ -z "$1" ]]; then
   echo "No arguments supplied :( "
   help_menu
@@ -20,6 +52,7 @@ else
     ;;
   "--create")
     echo "create records"
+    create_records
     ;;
   "--delete")
     echo "delete records"
