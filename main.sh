cf6ae6d127c51367eee12e9b43b972ab4edc12f7
n-kev1n <kevin_@tuta.io>
2022-05-28 12:23:38 +0200

    added script flags

--- main.sh
+++ main.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
+# Bash Script to create, search and delete records
 
-echo "Hello world"
+function help_menu() {
+  # Function to print out the help menu
+  echo "Consider:"
+  echo "    --create: To Add a New Record"
+  echo "    --delete: To Delete a Record"
+  echo "    --search: To Search for a Record"
+}
+
+if [[ -z "$1" ]]; then
+  echo "No arguments supplied :( "
+  help_menu
+else
+  echo $#
+  case "$1" in
+  "--help")
+    help_menu
+    ;;
+  "--create")
+    echo "create records"
+    ;;
+  "--delete")
+    echo "delete records"
+    ;;
+  "--search")
+    echo "search records"
+    ;;
+  *)
+    echo ":( Invalid Argument"
+    ;;
+  esac
+
+fi
