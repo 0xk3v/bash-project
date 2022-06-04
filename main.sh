64ca40cf72192d5988c3da674effd03b6db9f4bf
n-kev1n <kevin_@tuta.io>
2022-06-04 10:20:05 +0200

    implemented multiple authors

--- main.sh
+++ main.sh
@@ -22,9 +22,18 @@ check_dir
 
 # function to Create Records
 function create_records() {
-  echo "Enter the Author:"
-  read -r AUTHOR
-  echo "Would you like to Add another Authors??(y/n)"
+  local RECORDNAME=$(echo "$RANDOM$RANDOM$RANDOM.txt" | tr 'A-Z' 'a-z' | tr ' ' '_')
+
+  while [[ true ]]; do
+    echo "Enter the Author:"
+    read -r AUTHOR
+    echo "Would you like to Add another Author?(y/n)"
+    read -r ANS
+    if [[ $ANS == "n" ]]; then
+      break
+    fi
+    echo "Author: $AUTHOR" >>$BASEDIR/$RECORDNAME
+  done
 
   echo "Enter the Title:"
   read -r TITLE
@@ -33,13 +42,15 @@ function create_records() {
   echo "Enter the Year:"
   read -r YEAR
 
-  local RECORDNAME=$(echo "$TITLE $PAGES$YEAR$RANDOM.txt" | tr 'A-Z' 'a-z' | tr ' ' '_')
+  local RECORDNAME2=$(echo "$TITLE $PAGES$YEAR$RANDOM.txt" | tr 'A-Z' 'a-z' | tr ' ' '_')
 
   # Outputting Records to a file
   echo "Author: $AUTHOR" >>$BASEDIR/$RECORDNAME
   echo "Title: $TITLE" >>$BASEDIR/$RECORDNAME
   echo "Pages: $PAGES" >>$BASEDIR/$RECORDNAME
   echo "Year: $YEAR" >>$BASEDIR/$RECORDNAME
+
+  mv $BASEDIR/$RECORDNAME $BASEDIR/$RECORDNAME2
 
   echo "Records Successfully Created"
 }
